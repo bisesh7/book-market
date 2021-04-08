fa12af195c26173fd8ca5ebdf695e1f655353075
Bisesh Shakya <bisesh.shakya@gmail.com>
2021-04-08 14:26:04 +0545

    Added reacstrap, react-router-dom, bootstrap and added navbar

---
+++ src/Components/NavbarComponent.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from "reactstrap";
+
+const NavbarComponent = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Book-Market</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="https://github.com/bisesh7/book-market">
+                Github
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavbarComponent;
