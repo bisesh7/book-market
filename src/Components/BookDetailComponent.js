fc4b0bbfcc570b7a27839dc2dc2cb802f701d53a
Bisesh Shakya <bisesh.shakya@gmail.com>
2021-04-19 14:07:11 +0545

    Added the book context to the book detail page

--- src/Components/BookDetailComponent.js
+++ src/Components/BookDetailComponent.js
@@ -1,19 +1,41 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Col, Container, Row } from "reactstrap";
+import { BooksContext } from "../Contexts/BooksContext";
+import { getNumberFromString } from "../Functions/getNumberFromString";
 import CartComponent from "./CartComponent";
 import NavbarComponent from "./NavbarComponent";
 
 const BookDetailComponent = (props) => {
-  const [book] = useState(props.location.pathname);
+  const [bookId, setBookId] = useState();
 
-  console.log(book);
+  useState(() => {
+    try {
+      setBookId(getNumberFromString(props.match.params.book));
+    } catch (err) {
+      props.history.push("/");
+    }
+  }, []);
+
+  // Getting the books from context
+  const { books } = useContext(BooksContext);
+
+  const [book, setBook] = useState(null);
+
+  useEffect(() => {
+    const book = books.books.find((book) => book.id === bookId);
+    setBook(book);
+  }, [books]);
+
+  useEffect(() => {
+    console.log(book);
+  }, [book]);
 
   return (
     <div>
       <NavbarComponent {...props} />
       <Container>
         <Row>
-          <Col md="9">Book Detail</Col>
+          <Col md="9">{book ? book.id : null}</Col>
           <Col md="3">
             <strong>Cart</strong>
             <div className="sticky-top cart">
